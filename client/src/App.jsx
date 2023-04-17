e1d435ebb465614fdfaade7335247b47cf3e3e76
vishal <93590077+JaY072002@users.noreply.github.com>
2023-04-17 23:53:18 +0530

    client-server initial setup and auth implementation

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+function App() {
+  <Router>
+    <Routes>
+      <Route path="/" element={<Register />} />
+    </Routes>
+  </Router>;
+}
+
+export default App;
